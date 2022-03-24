79544a370fc255f3883a4765ad7d6ce79269392f
shyakadev <shyakadev@gmail.com>
2022-03-24 15:16:45 +0200

    ft: url shortener post and get

--- server.js
+++ server.js
@@ -1,24 +1,76 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+let shortUrlsDb = [];
+
+const isValidUrl = (string) => {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (e) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "http:";
+};
+
+const getUrls = (string) => {
+  let url;
+  if (isValidUrl(string)) {
+    !doesExist(string) ? saveUrl(string) : "";
+    url = doesExist(string);
+  } else {
+    url = { error: "Invalid URL" };
+  }
+
+  return url;
+};
+
+const doesExist = (string) =>
+  shortUrlsDb.find(({ original_url }) => original_url === string);
+
+const shortUrlDoesExist = (number) => {
+  const url = shortUrlsDb.find(({ short_url }) => short_url == number);
+  return url["original_url"];
+};
+
+const saveUrl = (string) =>
+  shortUrlsDb.push({ short_url: shortUrlsDb.length + 1, original_url: string });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+// First API endpoint
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.post("/api/shorturl", urlencodedParser, function (req, res) {
+  res.json(getUrls(req.body.url)), console.log(shortUrlsDb);
+});
+
+app.get("/api/shorturl/:short_url", function (req, res) {
+  try {
+    const url = shortUrlDoesExist(req.params.short_url);
+    res.redirect(url);
+  } catch (error) {
+    res.json({ error: "No short URL found for the given input" });
+  }
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
